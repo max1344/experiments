1cb7718e62d9fd0863eab100d1f133e1c3234a72
max1344 <maxhellner4@gmail.com>
2022-03-17 00:35:41 +0100

    wip

--- hangman.py
+++ hangman.py
@@ -1,8 +1,28 @@
 import random
 
-numbers = [1,2,3,4,5,6,7,8,9,10]
+
+playing = True
+
 
 
 def function():
-  
-  inp = input("Enter a number")
+  answer = random.randint(1,10)
+  inp = input("Guess your number: ")
+  while inp not in ["1", "2", "3","4","5","6","7","8","9","10"]:
+      #inp = input("Invalid guess again: ")
+      print("invalid")
+      return 
+  print("The correct answer was: " + str(answer))
+  if answer == int(inp):
+    print("correct answer")
+    playing = False
+  else: 
+    print("Wrong number")
+
+
+
+while playing == True:
+  function()
+
+
+
