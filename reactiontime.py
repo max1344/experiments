cb1e1dfcf7d7de4f7ad3ff98ffc5db9919e21c64
max1344 <maxhellner4@gmail.com>
2022-03-18 00:53:53 +0100

    wip

---
+++ reactiontime.py
@@ -0,0 +1,21 @@
+import time 
+import random
+from timeit import timeit 
+import time
+import 
+
+
+def reactiontime():
+    print("ready")
+    time.sleep(1)
+    print("set")
+    time.sleep(random.randint(2,5))
+    print("gooo")
+
+
+
+reactiontime()
+
+
+
+
