90c8adfa07a10d8fab3813fc3fe60a0cf36d3d15
max1344 <maxhellner4@gmail.com>
2022-03-27 21:42:53 +0200

    wip

--- dice roller.py
+++ dice roller.py
@@ -3,23 +3,23 @@ import random
 Playing = True
 
 def rolldice():
-    inp = input("Would you like to roll dice\nyes or no\n")
-    if inp == "yes":
-        print("You rolled: " + str(random.randint(1,6)))
-        playag = input("Would you like to roll again?")
-        print(playag)
+    while Playing == True:
+        inp = input("Would you like to roll dice\nyes or no\n")
+        if inp == "yes":
+            print("You rolled: " + str(random.randint(1,6)))
+            playag = input("Would you like to roll again?")
+            print(playag)
         if playag == "yes":
-            global Playing
             Playing = True
+            print("You rolled: " + str(random.randint(1,6)))
         else:
-            global Playing
             Playing = False
-    if inp == "no":
-        global Playing
-        Playing = False
-        print("why not?")
-        
+        if inp == "no":
+            Playing = False
+            print("why not?")
+            
 
         
-while Playing == True:
-    rolldice()
+
+
+rolldice()
