c869636cb3b4245aefa59cd9f2d589711b6ad611
max1344 <maxhellner4@gmail.com>
2022-03-28 11:00:07 +0200

    wip

--- guess the word.py
+++ guess the word.py
@@ -1,3 +1,10 @@
 import random 
 
+file = open('words.txt','r')
+words = file.readlines()
+for i in range(len(words)):
+    words[i] = words[i].replace("\n","")
 
+def guesstheword():
+    print(words[random.randint(0, len(words))])
+    
