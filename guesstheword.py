67a720633877d94b93452ba3e8be9cbb7f1ec7bb
max1344 <maxhellner4@gmail.com>
2022-03-28 13:39:11 +0200

    wip

--- guesstheword.py
+++ guesstheword.py
@@ -15,12 +15,28 @@ def LineSpam(amount):
     for i in range(amount):
         print("\n")
 
+def updateReveal(inp, reveal):
+    global active_word
+    for i, char in enumerate(active_word):
+        if inp == char:
+            reveal = reveal[:i] + inp + reveal[i+1:]
+    return reveal
+
+
 def guesstheword():
-    global playing
-    active_word = (words[random.randint(0, len(words))])
+    global playing, active_word
+    active_word = words[random.randint(0, len(words))]
     guesses = 10
+    reveal = ""
+    for i in range(len(active_word)):
+        reveal += "_"
     while playing == True:
-        print(len(str(active_word)))
+        if ("_" not in reveal):
+            print(reveal)
+            print("you won!")
+            #playing = False
+            break
+        print(reveal)
         inp = input("Enter a letter: ")
         LineSpam(50)
         print("you guessed: " + inp)
@@ -34,6 +50,8 @@ def guesstheword():
                 playing = False
                 print("Game Over ")
                 print(active_word)
+        reveal = updateReveal(inp, reveal)
+        
 
 
 guesstheword()
