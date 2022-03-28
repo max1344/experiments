b09971adbb116d623714e2eb922ac6a18d86b7ea
max1344 <maxhellner4@gmail.com>
2022-03-28 12:15:22 +0200

    wip

--- guess the word.py
+++ guess the word.py
@@ -5,6 +5,37 @@ words = file.readlines()
 for i in range(len(words)):
     words[i] = words[i].replace("\n","")
 
+#dog = "xdasds"
+ #if "x" is in dog:
+      #print "Yes!"
+
+playing = True
+
+def LineSpam(amount):
+    for i in range(amount):
+        print("\n")
+
 def guesstheword():
-    print(words[random.randint(0, len(words))])
+    global playing
+    active_word = "hello" #(words[random.randint(0, len(words))])
+    guesses = 10
+    while playing == True:
+        inp = input("Enter a letter: ")
+        LineSpam(50)
+        print("you guessed: " + inp)
+        if inp in active_word:
+            print("letter was in word")
+        if inp not in active_word:
+            print("letter not in word")
+            guesses -= 1
+            print("Number of guesses left: " + str(guesses))
+            if guesses <= 0:
+                playing = False
+                print("Game Over ")
+
+
+guesstheword()
+
+        
+    
     
