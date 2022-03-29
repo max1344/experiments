828c2c989e944fed6279b4fe73bfcc3a5581e220
max1344 <maxhellner4@gmail.com>
2022-03-29 10:33:42 +0200

    wip

--- pygame/dicerollerPyGame.py
+++ pygame/dicerollerPyGame.py
@@ -1,14 +1,16 @@
 from cgitb import text
+from tkinter.tix import Tree
 import pygame, sys
 pygame.init()
 pygame.display.set_caption("Arush")
 import random
 
-screen = pygame.display.set_mode((1920,1080))
+screen = pygame.display.set_mode((1100,600))
 FONT = pygame.font.Font(None,72)
 myfont = pygame.font.SysFont("monospace",100)
 color =(0,255,255)
 rectc = (255,0,0)
+visibleText = False
 
 while True:
     screen.fill(color)
@@ -17,13 +19,18 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                color = ((random.randint(0,255),random.randint(0,255), random.randint(0,255)))
-
+                visibleText = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                visibleText = False
+                #color = ((random.randint(0,255),random.randint(0,255), random.randint(0,255)))
+    if visibleText:
+        text1 = myfont.render("Hello World",500,rectc)
+        screen.blit(text1,(700,400))
     
     
-    #screen.fill(color)
-    #text1 = myfont.render("Hello World",500,rectc)
-    #screen.blit(text1,(700,400))
+    
+   
     #pygame.draw.rect(screen, rectc, pygame.Rect(100,100,100,100))
     pygame.display.update()
     
