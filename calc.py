821733ff79dce93a8812e218c837a5f3c7edf6ed
max1344 <maxhellner4@gmail.com>
2022-05-14 03:35:59 +0200

    wip

--- calc.py
+++ calc.py
@@ -1,27 +1,30 @@
 
-def addition (x,y,):
-     
-    return x+y
-
-def subtraktion(x,y):
-    return x-y
-
-def multiplikation(x,y):
-    return x*y
-
-def divition(x,y):
-    return x/y
-
-choice = input("Choose: \n1 additon   \n2 subtraktion \n3 multiplikation \n4 divition\n")
-x = int(input("Choose4 first number: "))
-y = int(input("Choose second number: "))
 
 
-if choice == "1":
-    print(addition(x,y))
-elif choice == "2":
-    print(subtraktion(x,y))
-elif choice == "3":
-    print(multiplikation(x,y))
-elif choice == "4":
-    print(divition(x,y))
+
+def calc(z,x,y):
+    if välj == "+":
+        res1 = x + y
+        print("Svar: " + str(res1))
+    if välj == "-":
+        res1 = x - y
+        print("Svar: " + str(res1))
+    if välj == "x":
+        res1 = x * y
+        print("Svar: " + str(res1))
+    if välj == "/":
+        res1 = x / y
+        print("Svar: " + str(res1))
+    else:
+        print("Invalid Character")
+
+välj = input("Välj räknesätt: +,-,x,/")
+inp1 = input("tal 1: ")
+inp2 = input("tal 2: ")
+calc(välj,int(inp1),int(inp2))
+
+
+    
+
+
+
